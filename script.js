78e0c4739055b507c99aa8c253297d7ddb057b3a
Sofia Pham <sofiapham98@gmail.com>
2020-10-20 09:19:25 -0400

    script2 for organizing csv file

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const superagent = require('superagent');
+const dataSource = 'https://instagram.com'
+
+const writeFile = (response) => {
+    // do something
+    const fs = require('fs')
+    const myError = (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!')
+      }
+    fs.writeFile('instagram.html', response.text, myError)
+}
+
+superagent.get(dataSource).then(writeFile)
